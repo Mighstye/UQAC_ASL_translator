ef44b1d71add4185f1e419faf99ebae86aeb3e6b
Romain Felgines <romain.felgines@gmail.com>
2022-04-05 02:26:40 -0400

    sorry

--- subtitles_extraction/main.py
+++ subtitles_extraction/main.py
@@ -9,16 +9,39 @@ if __name__ == '__main__':
 
 # C:\Users\romai\Dataset deeplearning
 # r'\FIXE_ROMAIN\Dataset deeplearning'
-    complete_file_list = os.listdir("C:\\Users\\romai\\DatasetDeeplearning")
+    # f = open(r"C:\Users\romai\DatasetDeeplearning\1176340_en.srt", encoding='utf8')
+    # print(f.read())
+
+    complete_file_list = os.listdir(r'C:\Users\romai\DatasetDeeplearning')
     filelist = []
     for file in complete_file_list:
-        if file.endswith('.mp4') and not file.endswith('_1a1.mp4') and not file.endswith('_1b1.mp4') \
-                and not file.endswith('_1c.mp4'):
-            file = file.replace('.mp4', '')
-            if not (file + '_1a1.mp4' in complete_file_list and file + '_1b1.mp4' in complete_file_list):
-                file = file + '_en.srt'
-                filelist.append(file)
+        if file.endswith('_1a1.mp4') or file.endswith('_1b1.mp4'):
+            filelist.append(file)
+    #print(filelist)
+
+    subs = pysrt.open()
+"""
     for file in filelist:
+        paragraphes = []
+        # subtitleNumber = []
+        f = open("C:/Users/romai/DatasetDeeplearning/" + file, encoding='utf-8')
+        lines = f.readlines()
+        print(lines)
+        for line in lines: # Separates paragraphes
+            if line == '/n':
+                paragraphes.append([])
+            else:
+                paragraphes[-1].append(line)
+        for paragraphe in paragraphes:
+            # subtitleNumber.append(paragraphe[0])
+            paragraphe[1] = [paragraphe[1][0:11]+ paragraphe[1][16:28]]
+            # ]
+            if not " " in paragraphe[2]: # If not a phrase
+                paragraphe[2] = paragraphe[2][3:]
+            paragraphes.append(paragraphe)
+        print(paragraphes)
+        
+        
         print(os.path.join("C:\\Users\\romai\\DatasetDeeplearning", file))
         subs = pysrt.open(os.path.join("C:\\Users\\romai\\DatasetDeeplearning", file))
         print(subs)
@@ -27,3 +50,5 @@ if __name__ == '__main__':
         fileName = fileName[0].split(".")
         filesName.append(fileName[0])
         allSubtitles.append(subs)
+        
+        """
